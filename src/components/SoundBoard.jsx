ed414a38c4dcbd7a5d78d9979607c6d5558b8836
Luciano Dichiara <luciano@studiohakuna.com>
2023-08-13 01:33:25 -0300

    Remove old code

--- src/components/SoundBoard.jsx
+++ src/components/SoundBoard.jsx
@@ -1,15 +1,8 @@
-// import { useState } from 'react'
 import './SoundBoard.css'
 
 import SoundButton from './SoundButton'
 
 export default function SoundBoard({ animals }) { 
-  //const [isSounding, setIsSounding] = useState(false)
-
-  // const handleStatus = () => {
-  //   setIsSounding((prevIsSounding) => !prevIsSounding);
-  // };
-
   const listAnimals = animals.map(animal =>
     <SoundButton animal={animal} key={animal.id} />
   )
